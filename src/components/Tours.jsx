6a7536cc8dd53f1735589ad39c21f53ee898dac6
efs0-cod3 <eflearnng@gmail.com>
2025-01-22 19:04:32 -0400

    jan22

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,3 +1,7 @@
+// {/* <button className='maintenance_nav--btn btn' onClick={goBack}> */}
+// {/* <BiLeftArrowAlt className='animate__animated animate__fadeIn'/> */}
+// {/* </button> */}
+
 const Tours = () => {
   return <div className="tours_page">Under construction</div>;
 };
--- src/data/toursData.js
+++ src/data/toursData.js
@@ -9,7 +9,7 @@ const toursData = [
     },{
       id: 2,
       title: "7 Iglesias Turquia",
-      description: "Sumérgete en la historia bíblica y recorre los caminos de los primeros cristianos con nuestro fascinante viaje a las 7 Iglesias del Apocalipsis. Este recorrido te conectará con las profundidades de la fe y la historia.",
+      description: "Sumérgete en la historia bíblica y recorre los caminos de los primeros cristianos con nuestro fascinante viaje a las 7 Iglesias del Apocalipsis. Este recorrido te conectará con las profundidades de la fe e historia.",
       price: 3385,
       image: "/assets/img/turkia/turkweb.png",
       duration: "11 dias",
